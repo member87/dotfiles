040bf9fca872e410d14d20c3aba666dced3798cf
jack <43416057+member87@users.noreply.github.com>
2022-01-28 22:21:17 +0000

    nvim git module changes

--- .config/nvim/lua/modules/init.lua
+++
@@ -1,8 +0,0 @@
-require('modules/lualine')
-require('modules/nvim-tree')
-require('modules/treesitter')
-require('modules/toggleterm')
-require('modules/telescope')
-require('modules/nvim-cmp')
-require('modules/brackets')
-require('modules/vgit')
